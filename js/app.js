f9827b41959f5ffe7093ed34012cd30801ac189b
fabricekx <fabricekx@gmail.com>
2024-02-02 11:43:42 +0100

    geolocation permission

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ function success(position) {
     const longitude = position.coords.longitude;
     console.log(position);
 
-
     // Update position HTML element
     document.getElementById("position").innerHTML = `Latitude: ${latitude}<br>Longitude: ${longitude}`;
 
@@ -21,14 +20,14 @@ function success(position) {
     }
 
     if (position.coords.heading !== null) {
-        // Retrieve speed
-        const heading= position.coords.heading;
+        // Retrieve heading
+        const heading = position.coords.heading;
 
-        // Update speed HTML element
-        document.getElementById("heading").innerHTML = `Cap: ${heading} deg`;
+        // Update heading HTML element
+        document.getElementById("heading").innerHTML = `Heading: ${heading} degrees`;
     } else {
         // Update heading HTML element if heading information is not available
-        document.getElementById("heading").innerHTML = 'heading  not available.';
+        document.getElementById("heading").innerHTML = 'Heading information not available.';
     }
 }
 
@@ -36,15 +35,23 @@ function error(error) {
     console.error(`Error getting geolocation: ${error.message}`);
 }
 
-// Button click event listener to start geolocation
-document.getElementById("startGeolocation").addEventListener("click", function() {
-    // Check if geolocation is supported by the browser
-    if ("geolocation" in navigator) {
-        // Watch for changes in geolocation
-        const watchId = navigator.geolocation.watchPosition(success, error);
-        // To stop watching position updates, you can use:
-        // navigator.geolocation.clearWatch(watchId);
-    } else {
-        console.error("Geolocation is not supported by this browser.");
-    }
-});
+if ("geolocation" in navigator) {
+    navigator.permissions.query({ name: 'geolocation' }).then(function (result) {
+        if (result.state === 'granted') {
+            // Permission already granted, proceed with geolocation
+            console.log("Geolocation permission granted");
+
+            // Request current position
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else if (result.state === 'prompt') {
+            // Permission not yet granted, prompt the user
+            console.log("Geolocation permission not granted, requesting permission...");
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            // Permission denied or unavailable
+            console.log("Geolocation permission denied or unavailable");
+        }
+    });
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
